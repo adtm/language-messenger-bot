a57d50c3f185c960e6716b07149ada62f52a8485
adtm <tomas.eglinskas@gmail.com>
2018-07-09 18:36:32 +0300

    test: add fake test

--- tests/main.js
+++ tests/main.js
@@ -4,4 +4,8 @@ describe("smoke test", function() {
   it("checks equality", function() {
     assert.equal(true, true);
   });
+
+  it("checks equality for false", function () {
+    assert.equal(false, false);
+  });
 });
