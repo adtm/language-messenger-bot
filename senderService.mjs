15ee5ae4c4f2db1f87d50b925a6a8b77f9abcb1e
adtm <tomas.eglinskas@gmail.com>
2018-07-14 12:44:10 +0300

    feat: initial dialogflow node-api
    current error message:
    
    Dialogflow API has not been used in project 764086051850 before or it is disabled. Enable it by visiting https://console.develope
    rs.google.com/apis/api/dialogflow.googleapis.com/overview?project=764086051850 then retry. If you enabled this API recently, wait a few minutes for the action to propagate to our systems and retry.'

--- senderService.mjs
+++ senderService.mjs
@@ -2,10 +2,49 @@ import request from "request";
 import dotenv from "dotenv";
 dotenv.config();
 
+
+const projectId = 'main-617b3';
+const sessionId = 'quickstart-session-id';
+
+
+import dialogflow from 'dialogflow';
+const sessionClient = new dialogflow.SessionsClient();
+const sessionPath = sessionClient.sessionPath(projectId, sessionId);
+
+
 const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 // Sends response messages via the Send API
 export function callSendAPI(sender_psid, response) {
-  // Construct the message body
+  console.log(response)
+
+  const dialogRequest = {
+  session: sessionPath,
+  queryInput: {
+    text: {
+      text: 'text',
+      languageCode: 'en-US',
+    },
+  },
+};
+
+  sessionClient
+  .detectIntent(dialogRequest)
+  .then(responses => {
+    console.log('Detected intent');
+    const result = responses[0].queryResult;
+    console.log(`  Query: ${result.queryText}`);
+    console.log(`  Response: ${result.fulfillmentText}`);
+    if (result.intent) {
+      console.log(`  Intent: ${result.intent.displayName}`);
+    } else {
+      console.log(`  No intent matched.`);
+    }
+  })
+  .catch(err => {
+    console.error('ERROR:', err);
+  });
+
+
   let request_body = {
     recipient: {
       id: sender_psid
