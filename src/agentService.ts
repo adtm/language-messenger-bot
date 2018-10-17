966b215cf11a60018633d87493d0de309dd6ac46
Paulius Kutka <kutka100@gmail.com>
2018-10-17 13:00:07 +0300

    Added falling back to agent and prettier config

--- src/agentService.ts
+++ src/agentService.ts
@@ -1,25 +1,29 @@
-import { Agent, MAIN_AGENT } from "./consts/agents.const";
+import { Agent, MAIN_AGENT, INTERVIEW_AGENT } from './consts/agents.const'
 
 //TODO: Convert to import - currently errors cause SessionClient() cannot accept strings;
-const dialogflow = require('dialogflow');
+const dialogflow = require('dialogflow')
 
 export default class AgentService {
-
-    private projectId: string;
-    private sessionId: string;
-    private languageCode = 'en-US';
-    private sessionPath;
-    private sessionClient: any;
-
-
+    private projectId: string
+    private sessionId: string
+    private languageCode = 'en-US'
+    private sessionPath
+    private sessionClient: any
 
     //TODO: Stack sessions and use them later.
     //TODO: Handle multiple users
-    constructor(agent: Agent = MAIN_AGENT) {
-        this.projectId = agent.project_id;
-        this.sessionClient = new dialogflow.SessionsClient({ keyFilename: agent.secret });
-        this.sessionId = Math.random().toString(36).substring(7);
-        this.sessionPath = this.sessionClient.sessionPath(this.projectId, this.sessionId);
+    constructor(agent: Agent = INTERVIEW_AGENT) {
+        this.projectId = agent.project_id
+        this.sessionClient = new dialogflow.SessionsClient({
+            keyFilename: agent.secret,
+        })
+        this.sessionId = Math.random()
+            .toString(36)
+            .substring(7)
+        this.sessionPath = this.sessionClient.sessionPath(
+            this.projectId,
+            this.sessionId
+        )
     }
 
     interactWithAgent(requestText) {
@@ -31,38 +35,56 @@ export default class AgentService {
                     languageCode: this.languageCode,
                 },
             },
-        };
+        }
 
         return this.sessionClient
             .detectIntent(request)
             .then(responses => {
-                const result = responses[0].queryResult;
+                const result = responses[0].queryResult
+                const isEndOfConversation = this.isEndOfConversation(result)
 
                 if (result.intent) {
-                    console.log(`  Intent: ${result.intent.displayName}`);
+                    console.log(`  Intent: ${result.intent.displayName}`)
 
                     return {
+                        isEndOfConversation,
                         intent: result.intent.displayName,
-                        response: result.fulfillmentText
+                        response: result.fulfillmentText,
                     }
                 } else {
-                    console.log(`  No intent matched.`);
+                    console.log(`  No intent matched.`)
 
                     return {
-                        response: result.fulfillmentText
+                        response: result.fulfillmentText,
                     }
-
                 }
             })
             .catch(err => {
-                console.error('ERROR:', err);
-            });
+                console.error('ERROR:', err)
+            })
     }
 
     changeAgent(agent: Agent) {
-        this.projectId = agent.project_id;
-        this.sessionClient = new dialogflow.SessionsClient({ keyFilename: agent.secret });
-        this.sessionId = Math.random().toString(36).substring(7);
-        this.sessionPath = this.sessionClient.sessionPath(this.projectId, this.sessionId);
+        this.projectId = agent.project_id
+        this.sessionClient = new dialogflow.SessionsClient({
+            keyFilename: agent.secret,
+        })
+        this.sessionId = Math.random()
+            .toString(36)
+            .substring(7)
+        this.sessionPath = this.sessionClient.sessionPath(
+            this.projectId,
+            this.sessionId
+        )
+    }
+
+    private isEndOfConversation(result) {
+        if (result.diagnosticInfo) {
+            console.log('end of converstaion object')
+            console.log(result.diagnosticInfo.fields.end_conversation)
+            return !!result.diagnosticInfo.fields.end_conversation
+        }
+
+        return false
     }
 }
--- src/handlers/intentHandler.ts
+++ src/handlers/intentHandler.ts
@@ -1,32 +1,29 @@
-import { SCENARIO } from "../consts/intent.const";
-import { CHOOSE_SCENARIO, INTERVIEW_CHOISE, RESTAURANT_BOOKING } from "../consts/quick-replies.const";
+import { SCENARIO } from '../consts/intent.const'
+import {
+    CHOOSE_SCENARIO,
+    INTERVIEW_CHOISE,
+    RESTAURANT_BOOKING,
+} from '../consts/quick-replies.const'
 
 export default class IntentHandler {
-
-    constructor() { }
+    constructor() {}
 
     handleIntent(payload) {
-        let response;
+        let response
 
         if (payload.intent === SCENARIO) {
-            response =
-                {
-                    "text": payload.response,
-                    "quick_replies": [
-                        {
-                            "content_type": "text",
-                            "title": RESTAURANT_BOOKING,
-                            "payload": CHOOSE_SCENARIO
-                        },
-                        {
-                            "content_type": "text",
-                            "title": INTERVIEW_CHOISE,
-                            "payload": CHOOSE_SCENARIO
-                        },
-                    ]
-                }
+            response = {
+                text: payload.response,
+                quick_replies: [
+                    {
+                        content_type: 'text',
+                        title: INTERVIEW_CHOISE,
+                        payload: CHOOSE_SCENARIO,
+                    },
+                ],
+            }
 
-            return response;
+            return response
         }
     }
 }
